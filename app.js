a1fe5a2237d3377a554409acb48fd787934b4937
Avinash KP <67153670+KPavinash@users.noreply.github.com>
2023-02-11 20:48:02 +0530

    Third  commit

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const requst= require('request');
 
-
 const app = express();
 
+app.use(express.static("public"));
+
+
+  app.get("/", function(req,res){
+    res.sendFile(__dirname+"/signup.html")
+
+  })
 
 
 app.listen(4000, function(){
