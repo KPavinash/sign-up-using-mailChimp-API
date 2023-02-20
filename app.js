b1cd1f64c99c4e0d347b1c2ea66ff774ac1105bb
Avinash KP <67153670+KPavinash@users.noreply.github.com>
2023-02-20 12:00:34 +0530

    gitignore added

--- app.js
+++ app.js
@@ -1,16 +1,44 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const requst= require('request');
+const request= require('request');
+const https= require('https');
 
 const app = express();
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 
   app.get("/", function(req,res){
     res.sendFile(__dirname+"/signup.html")
 
   })
+
+  app.post("/", function(req,res){
+    const firstname = req.body.fName;
+    const lastname = req.body.lName;
+    const email = req.body.email;
+
+    
+
+    var data={
+      members: [{
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstname,
+          LNAME: lastname
+        }
+
+      }]  }
+      var jsondata = JSON.stringify(data);
+      
+      https.request(url, options, function(response) {
+      
+      
+      
+      });
 
 
 app.listen(4000, function(){
